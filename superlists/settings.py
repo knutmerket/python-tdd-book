093a4dc040d3ef21dc161dd8248940d26bb9c8fd
knutmerket <dodraugen2k@hotmail.com>
2019-10-19 10:29:39 +0200

    ALLOWED_HOSTS back to normal

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,17 +25,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-SECRET_KEY = 'p=@a-5a5ok!+-jl(a508%h7139@(quz!bhs+j&rk=2u84x-#p8'
 ALLOWED_HOSTS = ['*']
 
-# if 'DJANGO_DEBUG_FALSE' in os.environ:
-#     DEBUG = False
-#     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-#     ALLOWED_HOSTS = [os.environ['SITENAME']]
-# else:
-#     DEBUG = True
-#     SECRET_KEY = 'insecure-key-for-dev'
-#     ALLOWED_HOSTS = []
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
